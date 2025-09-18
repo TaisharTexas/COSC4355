fcb02a6adeaf4ea4660431c8fc0c767388dc296c
Andrew <bayledgeacademy@icloud.com>
2025-09-18 16:56:09 -0500

    hw 3 added

---
+++ HW3_national_parks/NationalParksCatalog/Models/Views/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//
+//  Created by Andrew Lee on 9/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
